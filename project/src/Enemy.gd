864f39983fb913255b3dc9b39f5c288d0805224d
Andrew Stafford <astafford2@bsu.edu>
2020-11-23 14:36:38 -0500

    Change enemies to create different types
    moving and standstill

--- project/src/Enemy.gd
+++ project/src/Enemy.gd
@@ -12,19 +12,20 @@ onready var enemy_raycast = $RayCast2D
 
 
 func _physics_process(delta):
-	if _direction == 1:
-		enemy_sprite.flip_h = false
-	else:
-		enemy_sprite.flip_h = true
-	enemy_sprite.play("run")
-	
-	_e_velocity.y += GRAVITY * delta
-	_e_velocity.x = RUN_SPEED * _direction
-	_e_velocity = move_and_slide(_e_velocity, Vector2.UP)
-	
-	if is_on_wall():
-		_direction *= -1
-		enemy_raycast.position.x *= -1
-	if not enemy_raycast.is_colliding():
-		_direction *= -1
-		enemy_raycast.position.x *= -1
+	if get_node(".").name == "MovingEnemy":
+		if _direction == 1:
+			enemy_sprite.flip_h = false
+		else:
+			enemy_sprite.flip_h = true
+		enemy_sprite.play("run")
+		
+		_e_velocity.y += GRAVITY * delta
+		_e_velocity.x = RUN_SPEED * _direction
+		_e_velocity = move_and_slide(_e_velocity, Vector2.UP)
+		
+		if is_on_wall():
+			_direction *= -1
+			enemy_raycast.position.x *= -1
+		if not enemy_raycast.is_colliding():
+			_direction *= -1
+			enemy_raycast.position.x *= -1

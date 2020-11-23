f661f37ce5748fee8f23b6a48bdea6cb5ba640c2
Andrew Stafford <astafford2@bsu.edu>
2020-11-23 14:31:03 -0500

    Create basic level 4

---
+++ project/src/Level-4.gd
@@ -0,0 +1,145 @@
+extends Node2D
+
+
+onready var instructions_popup := $InstructionsPopup
+onready var pause_popup := $PausePopup
+
+onready var player := $Player
+onready var player_cam := $Player/PlayerCam
+onready var player_sprite := $Player/AnimatedSprite
+onready var queue_free_timer := $Player/QueueFreeTimer
+
+var enemy_count := 0
+onready var enemy_count_label := $InfoHUDLayer/EnemyCountLabel
+
+var soul_count := 0
+onready var soul_count_label := $InfoHUDLayer/SoulCountLabel
+onready var soul_group := $SoulGroup
+
+onready var parallax_background := $ParallaxBackground
+onready var level_cam := $LevelCam
+onready var game_over_lose_hud := $GameOverLoseHUD
+onready var game_over_win_hud := $GameOverWinHUD
+
+onready var death_sound_player := $DeathSoundPlayer
+onready var win_sound_player := $WinSoundPlayer
+
+onready var timer_hud := $InfoHUDLayer/TimeHUD
+onready var time_label := $InfoHUDLayer/TimeHUD/TimeLabel
+onready var seconds_timer := $InfoHUDLayer/TimeHUD/SecondsTimer
+onready var minutes_timer := $InfoHUDLayer/TimeHUD/MinutesTimer
+var seconds := 0
+var minutes := 0
+
+var main_scene_path := "res://src/TitleScreen.tscn"
+var credits_scene_path := "res://src/CreditsScreen.tscn"
+
+
+func _ready():
+	player.enable_level_3()
+	instructions_popup.popup()
+	
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		enemy_count += 1
+
+
+func _process(_delta):
+	if seconds <= 9:
+		time_label.text = str(minutes) + ":0" + str(seconds)
+	else:
+		time_label.text = str(minutes) + ":" + str(seconds)
+	
+	enemy_count_label.text = "Enemies left: " + str(enemy_count)
+	soul_count_label.text = "Souls collected: " + str(soul_count)
+	
+	if enemy_count <= 0:
+#		win_player()
+		pass
+	
+	if Input.is_action_just_pressed("pause_game"):
+		get_tree().paused = true
+		var screen_center = player_cam.get_camera_screen_center()
+		pause_popup.set_position(Vector2(screen_center.x-218.5, screen_center.y - 96.5))
+		pause_popup.show()
+
+
+func kill_player():
+	var death_screen_center = player_cam.get_camera_screen_center()
+	player.run_speed = 0
+	game_over(death_screen_center, "lose")
+	player_sprite.visible = false
+	player.explode_cogwheels()
+	death_sound_player.play()
+	seconds_timer.stop()
+	minutes_timer.stop()
+	queue_free_timer.start()
+
+
+func kill_enemy(body):
+	remove_child(body)
+	enemy_count -= 1
+	
+
+
+func win_player():
+	var win_screen_center = player_cam.get_camera_screen_center()
+	player.run_speed = 0
+	game_over(win_screen_center, "win")
+	win_sound_player.play()
+	seconds_timer.stop()
+	minutes_timer.stop()
+
+
+func game_over(screen_center, end_status):
+	var game_over_pos = Vector2(screen_center.x - 176.5, screen_center.y - 51)
+	level_cam.position = screen_center
+	level_cam.make_current()
+	
+	if end_status == "lose":
+		game_over_lose_hud.set_position(game_over_pos)
+	else:
+		game_over_win_hud.set_position(game_over_pos)
+
+
+func _on_Player_player_hit(body):
+	if body.is_in_group("enemies"):
+		kill_player()
+
+
+func _on_Player_player_area_hit(area):
+	if area.is_in_group("collectibles"):
+		soul_group.remove_child(area)
+		soul_count += 1
+
+
+func _on_Player_enemy_hit(body):
+	kill_enemy(body)
+
+
+func _on_RetryButton_pressed():
+	var _ignored = get_tree().reload_current_scene()
+
+
+func _on_MainMenuButton_pressed():
+	var _ignored = get_tree().change_scene(main_scene_path)
+
+
+func _on_SecondsTimer_timeout():
+	seconds += 1
+
+
+func _on_MinutesTimer_timeout():
+	minutes += 1
+
+
+func _on_QueueFreeTimer_timeout():
+	remove_child(player)
+
+
+func _on_Unpause_pressed():
+	pause_popup.hide()
+	get_tree().paused = false
+
+
+func _on_CreditsButton_pressed():
+	var _ignored = get_tree().change_scene(credits_scene_path)

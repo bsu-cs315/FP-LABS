8d9e32666542a0cd4553436d15e0d3c0a1e4041c
Andrew Stafford <astafford2@bsu.edu>
2020-12-09 20:04:27 -0500

    Add visibility notifier to queue free off screen fireball
    also add line to play animation when spawned

--- project/src/Fireball.gd
+++ project/src/Fireball.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
 
-const FLY_SPEED := 150
+const FLY_SPEED := 200
 var _velocity := Vector2()
+
+onready var fireball_sprite := $AnimatedSprite
 
 
 func _ready():
@@ -11,4 +13,10 @@ func _ready():
 
 func _physics_process(delta):
 	_velocity.x = FLY_SPEED	* delta
+	
 	translate(_velocity)
+	fireball_sprite.play("fireball")
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()

469c9706883b0eadb98178542457b097426654d9
Andrew Stafford <astafford2@bsu.edu>
2020-12-13 02:03:36 -0500

    Add function to projectile to set direction of flight

--- project/src/Fireball.gd
+++ project/src/Fireball.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 
 const FLY_SPEED := 200
 var _velocity := Vector2()
+var direction := 1
 
 onready var fireball_sprite := $AnimatedSprite
 
@@ -12,10 +13,16 @@ func _ready():
 
 
 func _physics_process(delta):
-	_velocity.x = FLY_SPEED	* delta
+	_velocity.x = FLY_SPEED * direction * delta
 	
 	translate(_velocity)
 	fireball_sprite.play("fireball")
+
+
+func set_fireball_direction(pass_dir):
+	direction = pass_dir
+	if pass_dir == -1:
+		fireball_sprite.flip_h = true
 
 
 func _on_VisibilityNotifier2D_screen_exited():

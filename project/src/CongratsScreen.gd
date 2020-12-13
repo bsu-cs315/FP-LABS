3435bf2e3c9b23164658d27cb158dbff216ea264
Andrew Stafford <astafford2@bsu.edu>
2020-12-13 18:46:51 -0500

    Update congrats script for unused delta

--- project/src/CongratsScreen.gd
+++ project/src/CongratsScreen.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 var credits_scene_path := "res://src/CreditsScreen.tscn"
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("congrats_continue"):
 		var _ignored = get_tree().change_scene(credits_scene_path)
